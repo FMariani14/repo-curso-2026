aa84c2e8810f4c75084b9827787f127f9ef068b7
Francesca <marianifrancesca05@outlook.com>
2026-09-04 00:57:45 -0300

    Tarea 02 - R Base: data types, numbers, strings, booleans, operators

---
+++ Tareas/Tarea02.R
@@ -0,0 +1,89 @@
+my_var <- 30
+my_var <- "Sally"
+x <- 10.5
+class(x)
+
+x <- 1000L
+class(x)
+
+x <- 9i + 3
+class(x)
+
+x <- "R is exciting"
+class(x)
+
+x <- TRUE
+class(x)
+
+x <- 10.5
+y <- 10L
+z <- 1i
+
+x <- 10.5
+y <- 55
+
+x
+y
+
+class(x)
+class(y)
+
+x <- 1000L
+y <- 55L
+
+x
+y
+
+class(x)
+class(y)
+
+x <- 3+5i
+y <- 5i
+
+x
+y
+
+class(x)
+class(y)
+
+x <- 1L
+y <- 2
+
+a <- as.numeric(x)
+b <- as.integer(y)
+
+x
+y
+
+class(a)
+class(b)
+
+"hello"
+'hello'
+
+str <- "Hello"
+str
+
+str <- "Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."
+
+str
+
+str1 <- "Hello"
+str2 <- "World"
+
+paste(str1, str2)
+
+10 + 5
+
+my_var <- 3
+
+my_var <<- 3
+
+3 -> my_var
+
+3 ->> my_var
+
+my_var
